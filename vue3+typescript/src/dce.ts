26d1c65bb200ee1a96de6a5eb7cf381848626973
Cube <877211593@qq.com>
2022-01-18 15:35:32 +0800

    update

--- vue3+typescript/src/dce.ts
+++
@@ -1,5 +0,0 @@
-import { CameraEnhancer } from "cube-dynamsoft-camera-enhancer";
-
-CameraEnhancer.engineResourcePath = "https://cdn.jsdelivr.net/npm/cube-dynamsoft-camera-enhancer@0.20220118131820.0/dist/";
-
-export default CameraEnhancer;
---
+++ vue3_typescript/src/dce.ts
@@ -0,0 +1,5 @@
+import { CameraEnhancer } from "cube-dynamsoft-camera-enhancer";
+
+CameraEnhancer.engineResourcePath = "https://cdn.jsdelivr.net/npm/cube-dynamsoft-camera-enhancer@0.20220117223407.0/dist/";
+
+export default CameraEnhancer;
