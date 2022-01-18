9c674dd19be99798f09fe8b62e53d7db9f61a84a
SHEN <62587615+shenwb123@users.noreply.github.com>
2022-01-18 10:38:42 +0800

    Update main.js

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import store from './store'
 
 Vue.config.productionTip = false
-Dynamsoft.DCE.CameraEnhancer.engineResourcePath = "https://unpkg.com/cube-dynamsoft-camera-enhancer@0.20220104111256.0/dist/"
+
 new Vue({
   el: '#app',
   store,
