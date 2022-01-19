67384763154c509bda51b127e80f6c29d4215f5f
SHEN <62587615+shenwb123@users.noreply.github.com>
2022-01-19 10:22:57 +0800

    Update index.jsx

--- react/src/Video/index.jsx
+++ react/src/Video/index.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
-import DCE from '../dce'
 import './index.css'
 
 export default class index extends Component {
+    state = {
+        enhancer: {}
+    }
+
     componentDidMount() {
         this.initSettings()
     }
 
     initSettings = async () => {
-        window.enhancer = await DCE.createInstance();
         const callbackCameraOpen = () => {
             console.log("camera opened");
         };
         // if close camera, should set the selection box to not selectable
         const callbackCameraClose = () => {
-            this.props.changeDisabled()
+            this.props.setSelectDisabled();
             console.log("camera closeed");
         };
         const callbackCameraChange = () => { 
@@ -23,10 +25,14 @@ export default class index extends Component {
         const callbackResolutionChange = () => {
             console.log("camera resolution changed");
         };
-        window.enhancer.on("cameraOpen", callbackCameraOpen);
-        window.enhancer.on("cameraClose", callbackCameraClose);
-        window.enhancer.on("cameraChange", callbackCameraChange);
-        window.enhancer.on("resolutionChange", callbackResolutionChange);
+        this.props.enhancer.then(r => {
+            this.setState({enhancer: r}, () => {
+                this.state.enhancer.on("cameraOpen", callbackCameraOpen);
+                this.state.enhancer.on("cameraClose", callbackCameraClose);
+                this.state.enhancer.on("cameraChange", callbackCameraChange);
+                this.state.enhancer.on("resolutionChange", callbackResolutionChange);
+            })
+        })
     }
 
     render() {
