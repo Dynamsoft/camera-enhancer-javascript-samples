8b3de217106ba431bf1d82daed181e623d87bf4c
Cube <877211593@qq.com>
2023-08-22 15:46:06 +0800

    remove unnecessary code;

--- hello-world/react/src/App.tsx
+++ hello-world/react/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './dce';
 import CameraEnhancerComponent from './components/CameraEnhancerComponent/CameraEnhancerComponent';
