27439886a90f24c3ce15a1487893b5a2d8c9247d
Cube <877211593@qq.com>
2023-08-24 16:08:02 +0800

    fix bugs; update base path;

--- hello-world/vue3/src/components/CameraEnhancerComponent.vue
+++ hello-world/vue3/src/components/CameraEnhancerComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref, getCurrentInstance, Ref } from "vue";
+import { onBeforeUnmount, onMounted, ref, getCurrentInstance, type Ref } from "vue";
 import {
   CameraView,
   CameraEnhancer,
