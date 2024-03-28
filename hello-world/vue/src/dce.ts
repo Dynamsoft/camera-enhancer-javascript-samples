250aca2e0315d9de78f3ff7ce152ed2a7abc6048
Keillion <keillionv@gmail.com>
2024-03-28 14:53:45 +0800

    4.0.2

--- hello-world/vue/src/dce.ts
+++ hello-world/vue/src/dce.ts
@@ -3,5 +3,5 @@ import { CoreModule } from 'dynamsoft-core';
 CoreModule.engineResourcePaths = {
   std: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-std@1.0.0/dist/",
   core: "https://cdn.jsdelivr.net/npm/dynamsoft-core@3.0.30/dist/",
-  dce: "https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@4.0.1/dist/"
+  dce: "https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@4.0.2/dist/"
 };
